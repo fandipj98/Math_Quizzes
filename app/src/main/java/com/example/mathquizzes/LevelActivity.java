cb3568dd7df576c99867d6984cf621b6bcaa9055
Fandi PJ <37364776+fandipj98@users.noreply.github.com>
2020-04-17 01:34:53 +0700

    tampilan halaman main menu dan list level

--- app/src/main/java/com/example/mathquizzes/LevelActivity.java
+++ app/src/main/java/com/example/mathquizzes/LevelActivity.java
@@ -6,16 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class LevelActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_level);
     }
 
-    public void play(View view){
-        Intent intent = new Intent(this, LevelActivity.class);
+    public void mainMenu(View view){
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
